e2c964039ed26eae9473046cfb6933e3c466331b
Arthur Deng <arthur.deng@ubtrobot.com>
2021-01-19 11:25:04 +0800

    update

--- index/index.js
+++ index/index.js
@@ -1,20 +1,29 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+let clients = [] // trying to finish differentiating clients
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    socket.broadcast.emit('joined', {})
+    socket.username = "anonymous";
+    // socket.broadcast.emit('joined', { online: onlineusers });
     socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message', msg);
+        socket.broadcast.emit('chat message', socket.username + ": " + msg);
     });
     socket.on('disconnect', () => {
         io.emit('left', {});
-        console.log('a user disconnected')
+        // clients.splice(onlineusers.indexOf(socket.username), 1);
+        // console.log(onlineusers);
     });
+    socket.on('change_username', (data) => {
+        socket.username = data.username;
+        // onlineusers.push(socket.username);
+    });
+    // console.log(onlineusers);
+
 });
 
 http.listen(3000, () => {
